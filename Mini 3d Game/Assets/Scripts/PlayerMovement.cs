dc62b441bc0861c39a40d598603a0cd57765f4ff
SnipLyfe <82203397+SnipLyfe@users.noreply.github.com>
2021-12-06 18:51:39 +0530

    Playermovement and FixedCamera Scripts

--- Mini 3d Game/Assets/Scripts/PlayerMovement.cs
+++ Mini 3d Game/Assets/Scripts/PlayerMovement.cs
@@ -3,34 +3,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-	// This is a reference to the Rigidbody component called "rb"
+
 	public Rigidbody rb;
 
-	public float forwardForce = 2000f;  // Variable that determines the forward force
-	public float sidewaysForce = 500f;  // Variable that determines the sideways force
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
 
-	// We marked this as "Fixed"Update because we
-	// are using it to mess with physics.
+
 	void FixedUpdate()
 	{
-		// Add a forward force
+
 		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-		if (Input.GetKey("d"))  // If the player is pressing the "d" key
+		if (Input.GetKey("d"))
 		{
-			// Add a force to the right
+
 			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 
-		if (Input.GetKey("a"))  // If the player is pressing the "a" key
+		if (Input.GetKey("a"))
 		{
-			// Add a force to the left
+
 			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 
-		if (rb.position.y < -6f)
-		{
-			FindObjectOfType<GameManager>().EndGame();
-		}
+
 	}
 }
