dd0639641192419e8819e2c87857a9b3a13a3581
SnipLyfe <82203397+SnipLyfe@users.noreply.github.com>
2021-12-06 21:56:46 +0530

    changes in playermovement script

--- Mini 3d Game/Assets/Scripts/PlayerMovement.cs
+++ Mini 3d Game/Assets/Scripts/PlayerMovement.cs
@@ -3,30 +3,35 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-
+	// This is a reference to the Rigidbody component called "rb"
 	public Rigidbody rb;
 
-	public float forwardForce = 2000f;
-	public float sidewaysForce = 500f;
+	public float forwardForce = 500f;  // Variable that determines the forward force
+	public float sidewaysForce = 500f;  // Variable that determines the sideways force
+	public float backwardForce = 500f;
 
-
+	// We marked this as "Fixed"Update because we
+	// are using it to mess with physics.
 	void FixedUpdate()
 	{
-
-		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-
-		if (Input.GetKey("d"))
+		if (Input.GetKey("w"))
 		{
-
-			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+			rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+		}
+		if (Input.GetKey("s"))
+		{
+			rb.AddForce(0, 0, -backwardForce * Time.deltaTime);
+		}
+		if (Input.GetKey("d"))  // If the player is pressing the "d" key
+		{
+			// Add a force to the right
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
 		}
 
-		if (Input.GetKey("a"))
+		if (Input.GetKey("a"))  // If the player is pressing the "a" key
 		{
-
-			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+			// Add a force to the left
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
 		}
-
-
 	}
 }
